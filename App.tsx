fa1b3e3d9c82afd5fc1be66c01537023af5d2b00
kzoeps <kzoepa@gmail.com>
2021-05-22 15:15:14 +0600

    feat: initial set-up with eslint husky and prettier

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -8,12 +17,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
