39bca8d4c582c59acb7d7a80a3ea8c36ba34fe66
kzoeps <kzoepa@gmail.com>
2021-05-23 15:50:23 +0600

    feat: firebase set up

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase'
+
+// REVISIT KARMA: Import only required modules once in production
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyA-2HCXx0jV5d9OIyTPOO1Xk6kLZubxm2M',
+    authDomain: 'first-mile-7bd71.firebaseapp.com',
+    projectId: 'first-mile-7bd71',
+    storageBucket: 'first-mile-7bd71.appspot.com',
+    messagingSenderId: '52636116402',
+    appId: '1:52636116402:web:31ce398c0fa3d7909630ab',
+    measurementId: 'G-EH7G5V096G',
+}
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export default { firebase }
