160655f3bd3fcb328b25b3d9114ddf3109c2970f
kzoeps <kzoepa@gmail.com>
2021-05-26 11:46:16 +0600

    feat: router navigations

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import 'react-native-gesture-handler'
+import SignUpScreen from 'src/screens/auth/SignUpScreen.ts'
 import LoginScreen from './src/screens/auth/LoginScreen'
 
 const Stack = createStackNavigator()
@@ -11,6 +12,7 @@ export default function App() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Login">
                 <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-const LoginScreen = () => (
+// eslint-disable-next-line react/prop-types
+const LoginScreen = ({ navigation }) => (
     <View>
         <Text>Hello World!</Text>
+        <Text onPress={() => navigation.navigate('SignUp')}>Go To Sign Up</Text>
     </View>
 )
 
