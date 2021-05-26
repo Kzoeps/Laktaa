f73b7c5fe67b62dcd43d38a86ee8402a0e10d5c4
kzoeps <kzoepa@gmail.com>
2021-05-26 12:51:23 +0600

    Revert "feat: set up api calls index and also set up routing"
    This reverts commit abbdd96532834afe69ea23a599917aa6688aa585.

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ const Stack = createStackNavigator()
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator>
                 <Stack.Screen name="Login" component={LoginScreen} />
             </Stack.Navigator>
         </NavigationContainer>
