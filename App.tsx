f4d1ff1024ea1fef25dba59357bf3a6dc26c4fce
kzoeps <kzoepa@gmail.com>
2021-05-30 14:27:48 +0600

    feat: store configuration

--- App.tsx
+++ App.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import 'react-native-gesture-handler'
+import { Provider } from 'react-redux'
 import LoginScreen from './src/screens/auth/LoginScreen'
 import SignUpScreen from './src/screens/auth/SignUpScreen'
+import store from './src/store/store'
 
 const Stack = createStackNavigator()
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Sign Up" component={SignUpScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Provider store={store}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                    <Stack.Screen name="Sign Up" component={SignUpScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
     )
 }
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -4,7 +4,9 @@ import { Text, View } from 'react-native'
 const LoginScreen = ({ navigation }) => (
     <View>
         <Text>Hello World!</Text>
-        <Text onPress={() => navigation.navigate('SignUp')}>Go To Sign Up</Text>
+        <Text onPress={() => navigation.navigate('Sign Up')}>
+            Go To Sign Up
+        </Text>
     </View>
 )
 
