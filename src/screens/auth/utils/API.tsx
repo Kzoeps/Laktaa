9ae50577c0f821c8fb6a3055df0b8ab3dcadc1fa
kzoeps <kzoepa@gmail.com>
2021-05-30 23:12:17 +0600

    feat: firebase auth calls been setup

--- src/screens/auth/utils/API.tsx
+++ src/screens/auth/utils/API.tsx
@@ -4,6 +4,10 @@ export default {
     signIn: (
         phoneNumber: string,
         applicationVerifier: firebase.auth.ApplicationVerifier
-    ) =>
+    ): Promise<firebase.auth.ConfirmationResult> =>
         firebase.auth().signInWithPhoneNumber(phoneNumber, applicationVerifier),
+    confirmCode: (
+        confirmation: firebase.auth.ConfirmationResult,
+        code: string
+    ): Promise<firebase.auth.UserCredential> => confirmation.confirm(code),
 }
