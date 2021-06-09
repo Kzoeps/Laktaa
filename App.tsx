cc04c998bea282e275c124f19ccc4258ff3fb3ec
kzoeps <kzoepa@gmail.com>
2021-06-09 23:08:02 +0600

    feat: seperated auth stack and navigation files
    for todo look at previous commit

--- App.tsx
+++ App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import { AuthProvider } from 'src/screens/auth/auth';
-import LoginScreen from './src/screens/auth/LoginScreen';
-import SignUpScreen from './src/screens/auth/SignUpScreen';
+import Routes from 'src/Routes.tsx.ts';
 import store from './src/store/store';
-
-const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <Provider store={store}>
-            <NavigationContainer>
-                <AuthProvider>
-                    <Stack.Navigator>
-                        <Stack.Screen name="Login" component={LoginScreen} />
-                        <Stack.Screen name="Sign Up" component={SignUpScreen} />
-                    </Stack.Navigator>
-                </AuthProvider>
-            </NavigationContainer>
+            <AuthProvider>
+                <Routes />
+            </AuthProvider>
         </Provider>
     );
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from 'src/screens/auth/AuthStack.tsx.ts';
 
-const Routes = (): JSX.Element => <NavigationContainer></NavigationContainer>;
+const Routes = (): JSX.Element => (
+    <NavigationContainer>
+        <AuthStack />
+    </NavigationContainer>
+);
+
+export default Routes;
