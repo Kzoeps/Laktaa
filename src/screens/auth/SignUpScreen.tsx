241ac1c5420049ac38600bd3f5a2317a1a6bffe1
kzoeps <kzoepa@gmail.com>
2021-06-14 22:27:17 +0600

    feat: wip formik use to create sign up form
    to do: create the password as well and the validation schema and then after it works create a text
    input component for it

--- src/screens/auth/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen.tsx
@@ -1,13 +1,20 @@
-import { Container, Content, Form, Input, Item, Label } from 'native-base';
+import { Button, Container, Content, Form, Input, Item, Label, Text } from 'native-base';
 import React, { FC } from 'react';
+import { useFormik } from 'formik';
 import { SIGN_UP_SCHEMA } from './models/constants';
 
 const SignUpScreen: FC = () => {
-  const initialFormValues = {
-    email: '',
-    password: '',
-  };
+	const initialValues = {
+		email: '',
+		password: '',
+	};
 
+	const formik = useFormik({
+		initialValues,
+		onSubmit: values => {
+			console.log(values);
+		}
+	})
   const signUpSchema = SIGN_UP_SCHEMA;
   return (
     <Container>
@@ -15,12 +22,26 @@ const SignUpScreen: FC = () => {
         <Form>
           <Item floatingLabel>
             <Label>Email</Label>
-            <Input />
+            <Input
+				onChangeText={formik.handleChange('email')}
+				onBlur={formik.handleBlur('email')}
+				value={formik.values.email}
+			/>
           </Item>
-          <Item floatingLabel last>
-            <Label>Password</Label>
-            <Input keyboardType="visible-password" />
-          </Item>
+          {/*<Item floatingLabel last>*/}
+          {/*  <Label>Password</Label>*/}
+          {/*  <Input*/}
+			{/*	name="password"*/}
+			{/*	type="password"*/}
+			{/*	onChange={formik.handleChange}*/}
+			{/*	value={formik.values.password}*/}
+			{/*/>*/}
+          {/*</Item>*/}
+			<Button onPress={formik.handleSubmit} block light>
+				<Text>
+					Sign Up
+				</Text>
+			</Button>
         </Form>
       </Content>
     </Container>
