ad1768e6f8440b09aa6f839bb9c5721f7ac96201
kzoeps <kzoepa@gmail.com>
2021-06-15 19:57:08 +0600

    feat: added validation schema for signup

--- src/screens/auth/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen.tsx
@@ -1,21 +1,33 @@
-import { Button, Container, Content, Form, Input, Item, Label, Text } from 'native-base';
+import {
+  Button,
+  Container,
+  Content,
+  Form,
+  Input,
+  Item,
+  Label,
+  Text,
+} from 'native-base';
 import React, { FC } from 'react';
 import { useFormik } from 'formik';
 import { SIGN_UP_SCHEMA } from './models/constants';
 
 const SignUpScreen: FC = () => {
-	const initialValues = {
-		email: '',
-		password: '',
-	};
+  const initialValues = {
+    email: '',
+    password: '',
+  };
 
-	const formik = useFormik({
-		initialValues,
-		onSubmit: values => {
-			console.log(values);
-		}
-	})
-  const signUpSchema = SIGN_UP_SCHEMA;
+  const validationSchema = SIGN_UP_SCHEMA;
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <Container>
       <Content>
@@ -23,25 +35,28 @@ const SignUpScreen: FC = () => {
           <Item floatingLabel>
             <Label>Email</Label>
             <Input
-				onChangeText={formik.handleChange('email')}
-				onBlur={formik.handleBlur('email')}
-				value={formik.values.email}
-			/>
+              onChangeText={formik.handleChange('email')}
+              onBlur={formik.handleBlur('email')}
+              value={formik.values.email}
+            />
           </Item>
-          {/*<Item floatingLabel last>*/}
-          {/*  <Label>Password</Label>*/}
-          {/*  <Input*/}
-			{/*	name="password"*/}
-			{/*	type="password"*/}
-			{/*	onChange={formik.handleChange}*/}
-			{/*	value={formik.values.password}*/}
-			{/*/>*/}
-          {/*</Item>*/}
-			<Button onPress={formik.handleSubmit} block light>
-				<Text>
-					Sign Up
-				</Text>
-			</Button>
+          {formik.touched.email && formik.errors.email && (
+            <Text>{formik.errors.email}</Text>
+          )}
+          <Item floatingLabel last>
+            <Label>Password</Label>
+            <Input
+              onChangeText={formik.handleChange('password')}
+              onBlur={formik.handleBlur('password')}
+              value={formik.values.password}
+            />
+          </Item>
+          {formik.touched.password && formik.errors.password && (
+            <Text>{formik.errors.password}</Text>
+          )}
+          <Button onPress={formik.handleSubmit} block light>
+            <Text>Sign Up</Text>
+          </Button>
         </Form>
       </Content>
     </Container>
