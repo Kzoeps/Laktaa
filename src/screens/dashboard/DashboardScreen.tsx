6991d6d5ab866953bc70f082b7ac23518637e016
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2021-07-15 20:35:20 +0600

    style: dashobard homepage overlay

--- src/screens/dashboard/DashboardScreen.tsx
+++ src/screens/dashboard/DashboardScreen.tsx
@@ -1,12 +1,15 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
-import { Button, Container, Content, Text } from 'native-base';
+import { TouchableOpacity, Dimensions } from 'react-native';
+import { Container, Content, Text, View } from 'native-base';
+import tailwind from 'tailwind-rn';
 import { AuthContext } from '../auth/auth';
-// import Navbar from '../../shared/components/Navbar/Navbar';
 import Pageheader from '../../shared/components/Pageheader/Pageheader';
 
 const DashboardScreen: FC = () => {
   const [shouldLogout, setShouldLogout] = useState<boolean>(false);
   const { logout } = useContext(AuthContext);
+  const data = {};
+
   useEffect(() => {
     if (shouldLogout) logout();
   }, [shouldLogout, logout]);
@@ -15,12 +18,31 @@ const DashboardScreen: FC = () => {
     <Container>
       <Content>
         <Pageheader navigation page="dashboard" />
-
-        <Text>THIS IS DASHBOARD SCREEN AND WORKS</Text>
-        <Button onPress={() => setShouldLogout(true)}>
-          <Text>Logout</Text>
-        </Button>
+        <View
+          style={[
+            tailwind('bg-white px-5 py-3 pb-3 -my-10 rounded-t-3xl '),
+            { height: Dimensions.get('window').height },
+          ]}
+        >
+          <Text>yo yo man </Text>
+          <Text>yo yo man </Text>
+          <Text>yo yo man </Text>
+          <Text>yo yo man </Text>
+          <Text>yo yo man </Text>
+        </View>
       </Content>
+
+      <View
+        style={tailwind(
+          'bg-green-400 px-5 py-3 text-center absolute bottom-0 w-full'
+        )}
+      >
+        <TouchableOpacity>
+          <Text style={tailwind('text-white text-center font-semibold')}>
+            Post Job
+          </Text>
+        </TouchableOpacity>
+      </View>
     </Container>
   );
 };
