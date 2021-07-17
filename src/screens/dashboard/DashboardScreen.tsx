ff743df8fb91ee28c7d220c81a2e709de01694ed
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2021-07-17 15:21:12 +0600

    style(dashboard job jumbotron): create cards for job display

--- src/screens/dashboard/DashboardScreen.tsx
+++ src/screens/dashboard/DashboardScreen.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
-import { TouchableOpacity, Dimensions } from 'react-native';
+import { TouchableOpacity, Dimensions, Image } from 'react-native';
 import { Container, Content, Text, View } from 'native-base';
 import tailwind from 'tailwind-rn';
 import { AuthContext } from '../auth/auth';
@@ -8,7 +8,78 @@ import Pageheader from '../../shared/components/Pageheader/Pageheader';
 const DashboardScreen: FC = () => {
   const [shouldLogout, setShouldLogout] = useState<boolean>(false);
   const { logout } = useContext(AuthContext);
-  const data = {};
+  const data = [
+    {
+      from_place: 'Thimphu',
+      to_place: 'Bumthang',
+      price: 100.0,
+      pieces: 3,
+      weight: '10kg',
+      phone: '17949642',
+      name: 'Phuntsho Norbu',
+      image: 'https://phuntshonorbu.com/images/gallery/Lungchutse/IMG_6846.jpg',
+    },
+    {
+      from_place: 'Pemagathsel',
+      to_place: 'Bumthang',
+      price: 4300.0,
+      pieces: 8,
+      weight: '50kg',
+      phone: '17942422',
+      name: 'Sangay Chezom',
+      image: 'https://phuntshonorbu.com/images/gallery/Lungchutse/IMG_6846.jpg',
+    },
+    {
+      from_place: 'Thimphu',
+      to_place: 'Mongar',
+      price: 1000.0,
+      pieces: 12,
+      weight: '23kg',
+      phone: '77889966',
+      name: 'Pema Dorji',
+      image: 'https://phuntshonorbu.com/images/gallery/Lungchutse/IMG_6846.jpg',
+    },
+    {
+      from_place: 'Lhuntse',
+      to_place: 'Samdrupjongkhar',
+      price: 789.0,
+      pieces: 6,
+      weight: '32kg',
+      phone: '17981235',
+      name: 'Sonam Zangmo',
+      image: 'https://phuntshonorbu.com/images/gallery/Lungchutse/IMG_6846.jpg',
+    },
+    {
+      from_place: 'Mongar',
+      to_place: 'Zhemgang',
+      price: 1235.0,
+      pieces: 3,
+      weight: '10kg',
+      phone: '17949642',
+      name: 'Phuntsho Norbu',
+      image: 'https://phuntshonorbu.com/images/gallery/Lungchutse/IMG_6846.jpg',
+    },
+    {
+      from_place: 'Thimphu',
+      to_place: 'Phuntsholing',
+      price: 3465.0,
+      pieces: 3,
+      weight: '7kg',
+      phone: '17171712',
+      name: 'Kuenzang Wangmo',
+      image: 'https://phuntshonorbu.com/images/gallery/Lungchutse/IMG_6846.jpg',
+    },
+    {
+      from_place: 'Paro',
+      to_place: 'Haa',
+      price: 550.0,
+      pieces: 1,
+      weight: '10kg',
+      phone: '17892317',
+      name: 'Dorji Gyeltshen',
+      image: 'https://phuntshonorbu.com/images/gallery/Lungchutse/IMG_6846.jpg',
+    },
+  ];
 
   useEffect(() => {
     if (shouldLogout) logout();
@@ -25,13 +96,37 @@ const DashboardScreen: FC = () => {
           ]}
         >
           <Text>yo yo man </Text>
-          <Text>yo yo man </Text>
-          <Text>yo yo man </Text>
-          <Text>yo yo man </Text>
-          <Text>yo yo man </Text>
+          {data.map((item) => (
+            <View
+              style={[
+                tailwind('bg-white m-2 px-5 py-3 flex flex-row rounded-3xl'),
+                {
+                  shadowColor: '#000',
+                  shadowOffset: {
+                    width: 0,
+                    height: 3,
+                  },
+                  shadowOpacity: 0.1,
+                  shadowRadius: 2.62,
+
+                  elevation: 4,
+                },
+              ]}
+            >
+              <View style={tailwind('w-20 border ')}>
+                <Image
+                  style={tailwind('w-24')}
+                  source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+                />
+              </View>
+
+              <View style={tailwind('flex-1 border')}>
+                <Text>{item.image}</Text>
+              </View>
+            </View>
+          ))}
         </View>
       </Content>
-
       <View
         style={tailwind(
           'bg-green-400 px-5 py-3 text-center absolute bottom-0 w-full'
