07ebe1da8bdb578e4333094ceb434d00621680b7
kzoeps <kzoepa@gmail.com>
2021-08-09 20:30:00 +0600

    feat: added spinner to user profile

--- src/screens/profile/UserProfile.tsx
+++ src/screens/profile/UserProfile.tsx
@@ -1,13 +1,14 @@
 import React,{ FC, useState } from 'react';
 import { View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import { Spinner } from 'native-base';
+import { selectUserDetails } from '../auth/store/authSlice';
 
 const UserProfile: FC = () => {
-
+	const userDetails = useSelector(selectUserDetails);
+	if (!userDetails.userName) return <Spinner accessibilityLabel='loading profile'/>
 	return (
 		<View>
-			<Text>
-				User Profile Works
-			</Text>
 		</View>
 	)
 }
