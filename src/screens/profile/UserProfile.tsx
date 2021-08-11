1edbdf9851be33640f654c67f6a2fca6e070976c
kzoeps <kzoepa@gmail.com>
2021-08-11 20:42:50 +0600

    feat: add update and cancel buttons to user profile

--- src/screens/profile/UserProfile.tsx
+++ src/screens/profile/UserProfile.tsx
@@ -1,11 +1,12 @@
 import React, { FC, useState } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { useSelector } from 'react-redux';
-import { Box, Spinner } from 'native-base';
+import { Box, Button, Icon, Spinner } from 'native-base';
 import { Formik, FormikProps, FormikValues } from 'formik';
 import { selectUserDetails } from '../auth/store/authSlice';
 import { EDIT_PROFILE_SCHEMA } from './models/constants';
 import FMTextInput from '../../shared/components/TextInput';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const UserProfile: FC = () => {
 	const [inputsDisabled, setInputsDisabled] = useState(true);
@@ -23,18 +24,34 @@ const UserProfile: FC = () => {
 			<Formik
 				initialValues={initialValues}
 				validationSchema={validationSchema}
-				onSubmit={({ name, password, location }) => {
-					console.log(name, password, location);
+				onSubmit={({ name, phoneNumber, location }) => {
+					setInputsDisabled(true);
+				}}
+				onReset={() => {
+					setInputsDisabled(true)
 				}}
 			>
 				{(formik: FormikProps<{ name: string, phoneNumber: number, location: string }>) => (
 					<>
 						<Box>
-							<FMTextInput disableInput={inputsDisabled} label='Name' name='name' formik={formik as unknown as FormikProps<FormikValues>} />
-							<FMTextInput disableInput={inputsDisabled} label='Location' name='location' formik={formik as unknown as FormikProps<FormikValues>} />
+							<FMTextInput disableInput={inputsDisabled} label='Name' name='name'
+													 formik={formik as unknown as FormikProps<FormikValues>} />
+							<FMTextInput disableInput={inputsDisabled} label='Location' name='location'
+													 formik={formik as unknown as FormikProps<FormikValues>} />
 							<FMTextInput disableInput={inputsDisabled} label='Phone Number' name='phoneNumber'
 													 formik={formik as unknown as FormikProps<FormikValues>} />
 						</Box>
+						{
+							inputsDisabled ? <Button endIcon={
+								<Icon as={<MaterialIcons name='arrow-forward' size='4' />} />
+							}
+																			 onPress={() => setInputsDisabled(false)}
+																			 light
+							>Edit</Button> : <>
+								<Button onPress={formik.handleSubmit}>Save</Button>
+								<Button onPress={() => {formik.resetForm(); setInputsDisabled(true)}}>Cancel</Button>
+							</>
+						}
 					</>
 				)}
 			</Formik>
