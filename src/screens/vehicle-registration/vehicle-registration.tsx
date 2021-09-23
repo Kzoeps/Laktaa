7500903a2b7840a87f8e5f52fb4abcd4a43e5de2
kzoeps <kzoepa@gmail.com>
2021-09-23 23:41:27 +0600

    fix: fix fuckin again and again api calls

--- src/screens/vehicle-registration/vehicle-registration.tsx
+++ src/screens/vehicle-registration/vehicle-registration.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext, useEffect, useState } from 'react';
+import React, { FC, useContext, useEffect, useRef, useState } from 'react';
 import { Box, Button, Spinner, useToast } from 'native-base';
 import { useDispatch, useSelector } from 'react-redux';
 import FMHeader from '../../shared/components/FMHeader/FMHeader';
@@ -8,32 +8,46 @@ import { DriverInfo, VehicleInfo } from './models/models';
 import { AuthContext } from '../auth/auth';
 import { getToastConfig, selectStoreStatus } from '../../shared/utils';
 import { APIStatuses, RootReducersEnum, ToastTypes } from '../../shared/models/model';
-import { setVehicleRegistration } from './store/driverSlice';
+import { getVehicleRegistrationDetails, setVehicleRegistration } from './store/driverSlice';
 import { RootState } from '../../store/store';
 
 const VehicleRegistration: FC = ({ navigation }) => {
-  const { currentUser } = useContext(AuthContext);
-  const [formValues, setFormValues] = useState<DriverInfo & VehicleInfo>();
-  const [updateDriverInfo, setUpdateDriverInfo] = useState<boolean>(false);
-  const vehicleInfo = useSelector((state: RootState) => state.vehicle.details);
-  const status = useSelector(selectStoreStatus(RootReducersEnum.vehicleSlice))
-  const dispatch = useDispatch();
-  const toast = useToast();
+	const { currentUser } = useContext(AuthContext);
+	const [formValues, setFormValues] = useState<DriverInfo & VehicleInfo>();
+	// const [updateDriverInfo, setUpdateDriverInfo] = useState<boolean>(false);
+	const updateDriverInfo = useRef<boolean>(false);
+	const vehicleInfo = useSelector((state: RootState) => state.vehicle.details);
+	const status = useSelector(selectStoreStatus(RootReducersEnum.vehicleSlice));
+	const dispatch = useDispatch();
+	const toast = useToast();
 
-  useEffect(() => {
-    if (updateDriverInfo) {
-      const { email } = currentUser;
-			const setDriverInfo = async (details: {vehicleInfo: VehicleInfo & DriverInfo, email: string} ) => {
-				await dispatch(setVehicleRegistration(details))
+	const setUpdateDriverInfo = (val: boolean) => updateDriverInfo.current = val;
+
+	useEffect(() => {
+		if (currentUser.email && !vehicleInfo) {
+			const getVehicleInfo = async (email: string) => {
+				await dispatch(getVehicleRegistrationDetails(email));
+			};
+			getVehicleInfo(currentUser.email);
+		}
+	}, [dispatch, formValues, currentUser.email, vehicleInfo]);
+
+	useEffect((): any => {
+		if (!updateDriverInfo.current) return;
+		const { email } = currentUser;
+		const setDriverInfo = async (details: { vehicleInfo: VehicleInfo & DriverInfo, email: string }) => {
+				await dispatch(setVehicleRegistration(details));
 				toast.show(
-					getToastConfig('Registered successfully', ToastTypes.success)
+					getToastConfig('Registered successfully', ToastTypes.success),
 				);
-				setUpdateDriverInfo(false);
-			}
-			// eslint-disable-next-line no-void
-      void setDriverInfo({vehicleInfo: formValues as DriverInfo & VehicleInfo, email});
-    }
-  }, [dispatch, updateDriverInfo, formValues, currentUser, toast]);
+				updateDriverInfo.current = false;
+		};
+		// eslint-disable-next-line no-void
+		setDriverInfo({ vehicleInfo: formValues as DriverInfo & VehicleInfo, email });
+		// eslint-disable-next-line no-void,no-return-assign,consistent-return
+		return (): void => void (updateDriverInfo.current = false);
+
+	}, [dispatch, updateDriverInfo.current, formValues, currentUser, toast]);
 
   if (status === APIStatuses.LOADING) return <Spinner accessibilityLabel="loading vehicle info"/>
   return (
