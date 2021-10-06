891ecc3cf4a7c01448b81e4ec2d36cd12329a0e7
kzoeps <kzoepa@gmail.com>
2021-10-06 22:24:50 +0600

    fix: ui fixes

--- src/screens/vehicle-registration/vehicle-form.tsx
+++ src/screens/vehicle-registration/vehicle-form.tsx
@@ -34,53 +34,54 @@ const VehicleForm: FC<{
 				>
 					{(formik: FormikProps<VehicleInfo & DriverInfo>) => (
 						<>
-							<View style={tailwind('w-full flex flex-row my-6 items-center')}>
 								<Box
-									style={tailwind('w-11/12 flex-row ')}
-									shadow={1}
-									_light={{backgroundColor: 'gray.700'}}
-									_dark={{backgroundColor: 'gray.700'}}
+									style={tailwind('w-11/12 items-center ml-4 my-10')}
+									shadow={3}
+									_light={{ backgroundColor: 'gray.50' }}
+									_dark={{ backgroundColor: 'gray.50' }}
 								>
-									<View style={tailwind('w-1/2 m-0')}>
-										<FMSelectInput
-											icon='car-cog'
-											formik={formik as unknown as FormikProps<FormikValues>}
-											name='vehicleType'
-											options={[...VEHICLE_TYPE_OPTIONS]}
-											placeholder='Vehicle Type'
-										/>
-										<FMTextInput
-											label='Model'
-											formik={formik as unknown as FormikProps<FormikValues>}
-											name='carModel'
-										/>
-										<FMTextInput
-											label='Color'
-											formik={formik as unknown as FormikProps<FormikValues>}
-											name='color'
-										/>
+									<View style={tailwind('w-11/12 flex-row my-5')}>
+										<View style={tailwind('w-1/2 m-0')}>
+											<FMSelectInput
+												icon='car-cog'
+												formik={formik as unknown as FormikProps<FormikValues>}
+												name='vehicleType'
+												options={[...VEHICLE_TYPE_OPTIONS]}
+												placeholder='Vehicle Type'
+											/>
+											<FMTextInput
+												label='Model'
+												formik={formik as unknown as FormikProps<FormikValues>}
+												name='carModel'
+											/>
+											<FMTextInput
+												label='Color'
+												formik={formik as unknown as FormikProps<FormikValues>}
+												name='color'
+											/>
+										</View>
+										<View style={tailwind('-ml-20 w-5/12')}>
+											<FMImageUploadDisplay callback={openCamera} label='Photo of car'
+																						styleProp='w-full'
+																						iconPlacement={<FontAwesome5 name='car-alt' size={24} color='black' />} />
+										</View>
 									</View>
-									<View style={tailwind('-ml-20 w-5/12')}>
-										<FMImageUploadDisplay callback={openCamera} label='Photo of car'
-																					styleProp='w-full'
-																					iconPlacement={<FontAwesome5 name='car-alt' size={24} color='black' />} />
-									</View>
+									<FMTextInput
+										label='BP-A-1234'
+										formik={formik as unknown as FormikProps<FormikValues>}
+										name='numberPlate'
+									/>
+									<FMTextInput
+										label='Blue Book number'
+										formik={formik as unknown as FormikProps<FormikValues>}
+										name='blueBookNumber'
+									/>
+									<FMTextInput
+										label='License Number'
+										formik={formik as unknown as FormikProps<FormikValues>}
+										name='licenseNumber'
+									/>
 								</Box>
-								<FMTextInput
-									label='BP-A-1234'
-									formik={formik as unknown as FormikProps<FormikValues>}
-									name='numberPlate'
-								/>
-								<FMTextInput
-									label='Blue Book number'
-									formik={formik as unknown as FormikProps<FormikValues>}
-									name='blueBookNumber'
-								/>
-								<FMTextInput
-									label='License Number'
-									formik={formik as unknown as FormikProps<FormikValues>}
-									name='licenseNumber'
-								/></View>
 							<FMTextInput
 								label='Driver Name'
 								formik={formik as unknown as FormikProps<FormikValues>}
