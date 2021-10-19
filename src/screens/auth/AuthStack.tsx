6ec5e5d65847ff364e39bd1e24429b7581dd6a67
kzoeps <kzoepa@gmail.com>
2021-10-19 23:38:11 +0600

    fix: initail setup for phone

--- src/screens/auth/AuthStack.tsx
+++ src/screens/auth/AuthStack.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './LoginScreen';
 import SignUpScreen from './SignUpScreen';
+import SignUpPhone from './SignUpPhone';
 
 const Stack = createStackNavigator();
 
 const AuthStack = (): JSX.Element => (
   <Stack.Navigator>
-    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Login" component={SignUpPhone} />
     <Stack.Screen
       name="Sign Up"
       component={SignUpScreen}
--- src/screens/auth/SignUpPhone.tsx
+++ src/screens/auth/SignUpPhone.tsx
@@ -1,16 +1,58 @@
-import React, { FC, useContext } from 'react';
+import { Formik, FormikProps, FormikValues } from 'formik';
+import React, { FC, useContext, useRef, useState } from 'react';
+import { Button } from 'native-base';
+import firebase from 'firebase';
+import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
+import { ApplicationVerifier } from '@firebase/auth-types';
+import FMTextInput from '../../shared/components/TextInput';
 import { AuthContext } from './auth';
-import LoginForm from './LoginForm';
 
 const SignUpPhone: FC = () => {
 	const { currentUser } = useContext(AuthContext);
+	const [verificationId, setVerificationId] = useState<string>('');
+	const recaptchaVerifier = useRef(null);
 	const initialValues = {
-		phone: '',
-		verificationCode: ''
+		phoneNumber: '',
+		verificationCode: '',
+	};
+
+	const sendVerification = (phoneNumber: string) => {
+		const phoneProvider = new firebase.auth.PhoneAuthProvider();
+		phoneProvider.verifyPhoneNumber(phoneNumber, recaptchaVerifier.current as unknown as ApplicationVerifier).then(setVerificationId);
 	}
+
+	const confirmCode = (code: string) => {
+		const credential = firebase.auth.PhoneAuthProvider.credential(
+			verificationId,
+			code
+		);
+		firebase.auth().signInWithCredential(credential).then((result) => {
+			debugger;
+		})
+	}
+
 	return (
 		<>
-			<LoginForm/>
+			<Formik initialValues={initialValues} onSubmit={({ phoneNumber, verificationCode }) => {
+				if (!verificationCode) {
+					sendVerification(phoneNumber);
+					return;
+				}
+				confirmCode(verificationCode);
+			}}>
+				{(formik: FormikProps<{ phoneNumber: string, verificationCode: string }>) => (
+					<>
+						<FMTextInput label='Phone Number' name='phoneNumber'
+												 formik={formik as unknown as FormikProps<FormikValues>} />
+						<FMTextInput label='Verification Code' name='verificationCode'
+												 formik={formik as unknown as FormikProps<FormikValues>} />
+						<FirebaseRecaptchaVerifierModal ref={recaptchaVerifier} firebaseConfig={firebase.app().options}/>
+						<Button onPress={formik.handleSubmit}>
+							Sign up
+						</Button>
+					</>
+				)}
+			</Formik>
 		</>
 	)
 }
