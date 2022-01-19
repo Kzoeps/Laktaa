5105e29d12ff10136a8492bfe9b6f3948cfae23e
kzoeps <kzoepa@gmail.com>
2022-01-19 22:00:32 +0600

    feat(app.tsx): change firestore configuration to laktaa

--- App.tsx
+++ App.tsx
@@ -8,13 +8,13 @@ import Routes from './src/Routes';
 import store from './src/store/store';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA-2HCXx0jV5d9OIyTPOO1Xk6kLZubxm2M',
-  authDomain: 'first-mile-7bd71.firebaseapp.com',
-  projectId: 'first-mile-7bd71',
-  storageBucket: 'first-mile-7bd71.appspot.com',
-  messagingSenderId: '52636116402',
-  appId: '1:52636116402:web:31ce398c0fa3d7909630ab',
-  measurementId: 'G-EH7G5V096G',
+	apiKey: "AIzaSyB6Zx41HqL6LlVaASQwNsXd4J28si0d-8E",
+	authDomain: "laktaa-bht.firebaseapp.com",
+	projectId: "laktaa-bht",
+	storageBucket: "laktaa-bht.appspot.com",
+	messagingSenderId: "271533523230",
+	appId: "1:271533523230:web:3038bf14cc2deac2b2490f",
+	measurementId: "G-9871Y71LLW"
 };
 
 firebase.apps.length ? firebase.app() : firebase.initializeApp(firebaseConfig);
